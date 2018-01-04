9c2a39f3bc29d7bcb5ec55627819e130e7717421
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-03 20:43:37 -0600

    numbers_to_words method created & waiting for a number

---
+++ test.rb
@@ -0,0 +1,10 @@
+def numbers_to_words(num)
+    
+end
+
+
+puts numbers_to_words(7)
+puts numbers_to_words(42)
+puts numbers_to_words(2001)
+puts numbers_to_words(1999)
+puts numbers_to_words(17999)
